67320000fa9628aaf444d1794a5f30ec8ce4f350
Matthew McEachen <matthew@photostructure.com>
2025-07-12 19:09:14 -0700

    wip(codegen): more work on regex patterns

--- src/generated/file_types/mod.rs
+++ src/generated/file_types/mod.rs
@@ -1,15 +1,5 @@
-//! File type detection generated code
-//!
-//! This module contains generated code for file type detection,
-//! including file type lookups and magic number patterns.
-
 pub mod file_type_lookup;
 pub mod magic_number_patterns;
 
-// Re-export commonly used functions
-pub use file_type_lookup::{
-    extensions_for_format, get_primary_format, resolve_file_type, supports_format,
-};
-pub use magic_number_patterns::{
-    get_magic_file_types, get_magic_number_pattern as get_magic_pattern,
-};
+pub use file_type_lookup::*;
+pub use magic_number_patterns::*;
