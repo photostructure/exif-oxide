15a6861aae105add1946a03f7fbedd33cc1b7231
Matthew McEachen <matthew@photostructure.com>
2025-08-01 22:15:56 -0700

    feat(codegen):Add  tag kits for FujiFilm_pm

--- src/generated/FujiFilm_pm/mod.rs
+++ src/generated/FujiFilm_pm/mod.rs
@@ -5,7 +5,9 @@
 
 pub mod ffmv_binary_data;
 pub mod main_model_detection;
+pub mod tag_kit;
 
 // Re-export all lookup functions and constants
 pub use ffmv_binary_data::*;
 pub use main_model_detection::*;
+pub use tag_kit::*;
