7eeb12553ed1a36629644ac757097c4ff00d1c90
Matthew McEachen <matthew@photostructure.com>
2025-08-03 11:02:57 -0700

    fix(undo-exiftool-patches): update checkout command to include all ExifTool Perl modules

--- scripts/undo-exiftool-patches.sh
+++ scripts/undo-exiftool-patches.sh
@@ -14,4 +14,7 @@ fi
 ROOT="$(cd "$(dirname "${BASH_SOURCE[0]}")"/.. >/dev/null 2>&1 && pwd)"
 cd "$ROOT"
 
-git -C ./third-party/exiftool checkout -- lib/Image/ExifTool/*.pm
+# Note! We don't just revert all of third-party/exiftool because the
+# exiftool-researcher may have written new docs in third-party/exiftool/docs
+# that we don't want to lose.
+git -C ./third-party/exiftool checkout -- lib/Image/ExifTool.pm lib/Image/ExifTool/*.pm lib/Image/ExifTool/*.pl
