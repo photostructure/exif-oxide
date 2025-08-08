d733bba55661e1a34cf017ea455a436ce8fc529e
Matthew McEachen <matthew@photostructure.com>
2025-08-07 19:48:15 -0700

    chore(codegen): delete config-based extraction system

--- codegen/src/generators/data_sets/mod.rs
+++
@@ -1,7 +0,0 @@
-//! Boolean membership testing data sets
-//! 
-//! This module handles generation of HashSet-based boolean membership tests
-//! like weak_magic_types which just need to check if a key exists.
-
-pub mod boolean;
-
