1a24b01862af1634f7da7e563ad6f4fac243b216
Matthew McEachen <matthew@photostructure.com>
2025-08-09 14:14:54 -0700

    chore(codegen): final result of new codegen

--- src/generated/Font_pm/mod.rs
+++
@@ -1,9 +0,0 @@
-//! Generated lookup tables from Font.pm
-//!
-//! Auto-generated from third-party/exiftool/lib/Image/ExifTool/Font.pm
-//! DO NOT EDIT MANUALLY - changes will be overwritten by codegen
-
-pub mod tag_kit;
-
-// Re-export all lookup functions and constants
-pub use tag_kit::*;
