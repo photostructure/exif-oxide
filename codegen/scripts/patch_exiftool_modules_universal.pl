a905f0cad0024b2f85bd36ed67c09fc5b6ab7e89
Matthew McEachen <matthew@photostructure.com>
2025-08-09 14:12:01 -0700

    refactor: improve variable conversion logic in patch_exiftool_modules_universal.pl

--- codegen/scripts/patch_exiftool_modules_universal.pl
+++ codegen/scripts/patch_exiftool_modules_universal.pl
@@ -51,47 +51,85 @@ sub convert_all_my_to_package_variables {
     close($fh);
 
     my $modified = 0;
-    my @converted_vars;
 
-    # Find all top-level 'my %var' or 'my @var' declarations first
-    # (before we start modifying the content)
-    while ( $content =~ /^(\s*)my\s+([%@])(\w+)\s*=/gm ) {
-        my $sigil = $2;
-        my $name = $3;
-        push @converted_vars, [$sigil, $name];  # Store [sigil, name] as array ref
+    # Find and convert ONLY package-level 'my %var' or 'my @var' declarations
+    # (not ones inside subroutines)
+    my @vars_to_export;
+
+    # Split content into lines for analysis
+    my @lines       = split /\n/, $content;
+    my $in_sub      = 0;
+    my $brace_count = 0;
+
+    for ( my $i = 0 ; $i < @lines ; $i++ ) {
+        my $line = $lines[$i];
+
+        # Track whether we're inside a subroutine
+        if ( $line =~ /^\s*sub\s+\w+/ ) {
+            $in_sub      = 1;
+            $brace_count = 0;
+        }
+
+        # Count braces to track when we exit a sub
+        if ($in_sub) {
+            $brace_count += ( $line =~ tr/{/{/ ) - ( $line =~ tr/}/}/ );
+            if ( $brace_count <= 0 && $line =~ /}/ ) {
+                $in_sub = 0;
+            }
+        }
+
+        # Only collect variables declared at package level (not in subs)
+        # Must start at beginning of line with no indentation
+        if ( !$in_sub && $line =~ /^my\s+([%@])(\w+)\s*=/ ) {
+            push @vars_to_export,
+              {
+                sigil    => $1,
+                name     => $2,
+                line_num => $i
+              };
+        }
     }
 
-    # Now perform the conversions
-    if (@converted_vars) {
-        # Convert all my to our
-        $content =~ s/^(\s*)my(\s+[%@]\w+\s*=)/$1our$2/gm;
+    if (@vars_to_export) {
+        print STDERR "Converting "
+          . scalar(@vars_to_export)
+          . " package-level variables from 'my' to 'our'\n"
+          if $ENV{DEBUG};
+
+        # Convert only package-level my declarations to our
+        # Build a pattern that matches only our specific variables
+        my $var_pattern =
+          join( '|', map { quotemeta( $_->{name} ) } @vars_to_export );
+
+        # Only match lines with no indentation (^my not ^\s*my)
+        $content =~ s/^my(\s+[%@](?:$var_pattern)\s*=)/our$1/gm;
         $modified = 1;
-        
-        # Add symbol table aliases at the end of the package
-        # Find where to insert (before the final "1;" or at end)
-        my $insert_point = rindex($content, "\n1;");
-        
-        my $aliases = "\n# Ensure the field_extractor can see our exported fields:\n";
-        foreach my $var (@converted_vars) {
-            # Ensure we have an array ref
-            if (ref($var) ne 'ARRAY') {
-                die "Expected array ref but got: " . (ref($var) || "scalar value '$var'") . "\n";
-            }
-            my ($sigil, $name) = @$var;
-            if ($sigil eq '%') {
-                $aliases .= "*$name = \\%$name;\n";
-            } elsif ($sigil eq '@') {
-                $aliases .= "*$name = \\@$name;\n";
-            }
+
+        # Build symbol table export statements
+        my $exports =
+          "\n# Ensure the field_extractor can see our exported fields:\n";
+        foreach my $var (@vars_to_export) {
+            my $sigil = $var->{sigil};
+            my $name  = $var->{name};
+            $exports .= "*$name = \\${sigil}$name;\n";
         }
-        
-        if ($insert_point > -1) {
-            # Insert before the "1;"
-            substr($content, $insert_point, 0, $aliases);
-        } else {
-            # No "1;" found, append at end
-            $content .= $aliases;
+
+        # Find insertion point - at the very end of Perl code
+        # Either before __END__ if it exists, or at the end of the file
+        my $end_marker_pos = index( $content, "\n__END__" );
+
+        my $insert_point;
+        if ( $end_marker_pos > -1 ) {
+
+            # Insert before __END__
+            $insert_point = $end_marker_pos;
         }
+        else {
+            # No __END__, append at the very end
+            $insert_point = length($content);
+        }
+
+        substr( $content, $insert_point, 0, $exports );
     }
 
    # Add marker for AddCompositeTags calls
