98425b7fafdb81b493be692ca4fac0114d892d9c
Matthew McEachen <matthew@photostructure.com>
2025-08-28 00:15:11 -0700

    feat(scripts): enhance JSON output by adding count of unique conditions and conversions

--- scripts/uniq-print-conv.sh
+++ scripts/uniq-print-conv.sh
@@ -12,4 +12,4 @@ for file in $(jq -r '.modules | to_entries | map(.value[]) | .[]' config/exiftoo
   # quite as overwhelming. It's still gigantic: 22k lines of JSON!
 
   perl ./codegen/scripts/field_extractor.pl "third-party/exiftool/$file" | jq --arg mod "$module_name" '.data[] | if type == "array" then .[0:4][] else . end | select(type == "object" and has("PrintConv")) | {Name: ($mod + "." + .Name), PrintConv: (if (.PrintConv | type) == "object" then (.PrintConv | to_entries | .[0:4] | from_entries) else .PrintConv end)}'
-done | jq -s 'group_by(.PrintConv) | map({PrintConv: .[0].PrintConv, Names: [.[].Name], Count: length}) | sort_by(.PrintConv | length)'
+done | jq -s 'group_by(.PrintConv) | map({PrintConv: .[0].PrintConv, Names: [.[].Name], Count: length}) | sort_by(.Count) | reverse'
