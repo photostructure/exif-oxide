16a7d5f9665a54affaf4907e1097f33dea048adc
Matthew McEachen <matthew@photostructure.com>
2025-08-28 00:15:11 -0700

    feat(scripts): enhance JSON output by adding count of unique conditions and conversions

--- scripts/uniq-condition.sh
+++ scripts/uniq-condition.sh
@@ -10,4 +10,4 @@ cd "$(dirname "${BASH_SOURCE[0]}")/.."
 for file in $(jq -r '.modules | to_entries | map(.value[]) | .[]' config/exiftool_modules.json); do
   module_name=$(echo "$file" | sed 's|.*/||' | sed 's|\.pm$||')
   perl ./codegen/scripts/field_extractor.pl "third-party/exiftool/$file" | jq --arg mod "$module_name" '.data[] | if type == "array" then .[] else . end | select(type == "object" and has("Condition")) | {Name: ($mod + "." + .Name), Condition: .Condition}'
-done | jq -s 'group_by(.Condition) | map({Condition: .[0].Condition, Names: [.[].Name]}) | sort_by(.Condition | length)'
+done | jq -s 'group_by(.Condition) | map({Condition: .[0].Condition, Names: [.[].Name], Count: length}) | sort_by(.Count) | reverse'
